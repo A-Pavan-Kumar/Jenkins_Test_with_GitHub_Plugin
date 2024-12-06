8d97a59945b96d986b876b450942382b79035f56
A-Pavan-Kumar <125584006+A-Pavan-Kumar@users.noreply.github.com>
2024-12-06 20:10:07 +0530

    Create Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    time_t now = time(0);
+    char* dt = ctime(&now);
+
+    cout << "Current date and time: " << dt << endl;
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
